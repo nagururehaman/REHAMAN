c93c4466e58270bf39c427da4a1191f088735b36
REHAMAN NAGURU ABDUR <37996078+nagururehaman@users.noreply.github.com>
2022-02-09 11:42:23 -0600

    my cal code files

---
+++ NaguruAbdur_Calculator/NaguruAbdur_Calculator/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  NaguruAbdur_Calculator
+//
+//  Created by Naguru Abdur,Rehaman on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var outLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func acbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func divisonButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplicationButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func subButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func addButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func remButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func equalsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func dotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func zeroButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func oneButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func twoButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func threeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func fourButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func sixButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func sevenButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func eightButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func nineButton(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
